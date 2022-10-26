1deb376f4dfd5bf3a7dc3458760c712aa0b3dbec
Hossein <54559577+hosseinkavianih@users.noreply.github.com>
2022-10-26 11:00:47 -0400

    Update sample_parameters.sh

--- Hyperparameters/sample_parameters.sh
+++ Hyperparameters/sample_parameters.sh
@@ -1,6 +1,6 @@
 NSAMPLES=1000
 METHOD=Latin
-JAVA_ARGS="-cp /gpfs/gpfs0/project/quinnlab/hk3sku/Scratch-Old/hk3sku/MCMC/Parameters/MOEAFramework-2.13-Demo.jar"
+JAVA_ARGS="Bayesian/Hyperparameters/MOEAFramework-2.13-Demo.jar"
 
 # Generate the parameter samples
 echo -n "Generating parameter samples..."
