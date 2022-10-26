56aac700010127e06d8e68b8085b035c59368873
Daniel Lassiter <64621312+lassiterdc@users.noreply.github.com>
2022-10-26 11:30:26 -0400

    Update SMC.sh

--- Bimodal/SMC.sh
+++ Bimodal/SMC.sh
@@ -14,6 +14,14 @@
 # note that the folders '_script_outputs' and '_script_errors' must exist beforehand
 # in the same directory as the bash script that you run
 # this will create a single output and error file for each script
+# then you can use helpful commands like:
+# Helpful SLURM commands:
+    # sacct -j [job id #] -o jobid,jobname%20,user,partition,state,start,end,NodeList%60
+    # - this is helpful for getting the cluster’s estimate of when a job will start (start command); you can also submit this for completed jobs to get the wall clock time
+    # - This came in handy when trying to balance memory allocation requests (larger requests would cause a longer wait time) and data chunk size (larger chunks would mean faster computing time)
+
+    # seff [job id #]
+    # - returns job statistics, e.g., CPU and memory efficiency
 
 module purge
 module load goolf/7.1.0_3.1.4  R
