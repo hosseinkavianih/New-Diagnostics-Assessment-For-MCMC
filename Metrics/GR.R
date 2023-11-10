74f2870130a55673ce7dc8e884cb305f5fb3d1aa
Hossein <54559577+hosseinkavianih@users.noreply.github.com>
2023-11-10 07:21:26 -0500

    Update GR.R

--- Metrics/GR.R
+++ Metrics/GR.R
@@ -1,1 +1,36 @@
 
+arg=commandArgs(T)
+library(BayesianTools)
+
+k <- strtoi(arg[1]) + 1
+
+setwd("/gpfs/gpfs0/project/quinnlab/hk3sku/MCMC/bimodal/MHNew2")
+
+psrffinal <- matrix(NA,1000,25)
+
+for (j in 1:1000) {
+
+ Pattern <- paste("HyperparameterSeedParam",j,".Rdata",sep="")
+ lofparam <- list.files(pattern = Pattern)
+ if (length(lofparam) > 0) {
+ 
+ load(Pattern)
+ psrfmatrix <-matrix(NA,25,1)
+ 
+ for (i in 1:length(listdata)) {
+ 
+  tryCatch({
+  print(j)
+  print(i)
+  psrfmatrix[i] <- gelmanDiagnostics(listdata[[i]])$psrf[k,1]
+  print(psrfmatrix[i])
+  }, error=function(e){cat("ERROR :",conditionMessage(e), "\n")})
+  }
+  tryCatch({
+  psrffinal[j,] <- psrfmatrix
+  print(psrffinal[j,])
+  }, error=function(e){cat("ERROR :",conditionMessage(e), "\n")})
+ }
+}
+PSRFparam1 <- paste("/gpfs/gpfs0/project/quinnlab/hk3sku/MCMC/bimodal/GelmanMH/GelmanParam", k, ".txt",sep="")
+write.table(psrffinal,file = PSRFparam1)
